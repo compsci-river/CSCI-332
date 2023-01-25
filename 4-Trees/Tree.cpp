dc0685ed211f05d57311248d6063863bbbea7e61
River Sheppard <compsci.river@gmail.com>
2023-01-25 14:23:10 -0700

    Binary tree
    Some work, some testing.
    Still need to implement the array based. May need to add more functions to the reference based.

--- 4-Trees/Tree.cpp
+++ 4-Trees/Tree.cpp
@@ -21,6 +21,22 @@ struct Node{
     Node(int _data){
         data = _data;
     }
+
+    bool hasLeftChild(){
+        return left != NULL;
+    }
+
+    bool hasRightChild(){
+        return right != NULL;
+    }
+
+    bool isInternal(){
+        return hasLeftChild()||hasRightChild();
+    }
+
+    bool hasTwoChildren(){
+        return hasLeftChild()&&hasRightChild();
+    }
 };
 
 class RefTree{
@@ -30,36 +46,127 @@ class RefTree{
         return root = NULL;
     }
 
-    public:
-        void add(int x){
-            if(isEmpty()){
-                root = new Node(x);
+    Node* add(int x, Node* node){
+        if(node == NULL){
+            cout << "Added " << x << " to the tree." << endl;
+            return new Node(x);
+        }else{
+            if(x < node->data){
+                node->left = add(x, node->left);
+                return node;
+            }else if(x > node->data){
+                node->right = add(x, node->right);
+                return node;
             }else{
-                Node* prev;
-                Node* cur = root;
-                bool dir;
-                while(cur != NULL){
-                    prev = cur;
-                    if(x < cur->data){
-                        cur = cur->left;
-                        dir = true;
-                    }else if(x > cur->data){
-                        cur = cur->right;
-                        dir = false;
+                cout << x << " already exists in the tree." << endl;
+                return node;
+            }
+        }
+    }
+
+    Node* remove(int x, Node* node){
+        if(node == NULL){
+            //if the recusion reaches a null node, then that means the value x does not exist within the tree
+            cout << x << " does not exist in the tree." << endl;
+            return NULL;
+        }else{
+            //Searching for the node of value x
+            if(x < node->data){
+                //Current node value is larger than x, continue search in left node
+                node->left = remove(x, node->left);
+                return node;
+            }else if(x > node->data){
+                //Current node value is smaller than x, continue search in right node
+                node->right = remove(x, node->right);
+                return node;
+            }else{
+                //Found the node of value x, onto deleting it
+                if(node->isInternal()){
+                    if(node->hasTwoChildren()){
+                        //if the node has two children then it must be replaced by the leftmost descendant of the right child (could also do the rightmost descendant of the
+                        //left child, but the methods are equal so it didn't matter which one I picked.)
+                        Node* prev = node->right;
+                        Node* cur = node->right;
+                        while(cur->left != NULL){
+                            prev = cur;
+                            cur = cur->left;
+                        }
+                        if(cur != prev){
+                            prev->left = cur->right;
+                            cur->right = node->right;
+                        }
+                        cur->left = node->left;
+                        delete node;
+                        cout << "Removed " << x << " from the tree." << endl;
+                        return cur;
                     }else{
-                        cout << x << "already exists in the tree." << endl;
-                        return;
+                        //if the node only has one child than the child just gets moved into its spot
+                        if(node->hasLeftChild()){
+                            Node* left = node->left;
+                            delete node;
+                            cout << "Removed " << x << " from the tree." << endl;
+                            return left;
+                        }else{
+                            Node* right = node->right;
+                            delete node;
+                            cout << "Removed " << x << " from the tree." << endl;
+                            return right;
+                        }
                     }
-                }
-                if(dir){
-                    cur->left = new Node(x);
                 }else{
-                    cur->right = new Node(x);
+                    //If the node is external it just needs to be deleted and return null so there is no longer a pointer to it.
+                    delete node;
+                    cout << "Removed " << x << " from the tree." << endl;
+                    return NULL;
                 }
             }
         }
+    }
+
+    void print(Node* node, int space){
+        if(node == NULL){
+            return;
+        }else{
+            space++;
+            print(node->left,space);
+            for(int i = 0; i < space; i++){
+                cout << "\t";
+            }
+            cout << node->data << endl;
+            print(node->right,space);
+        }
+    }
+
+    public:
+        void add(int x){
+            cout << "Attempting to add " << x << " to the tree." << endl;
+            root = add(x, root);
+            print();
+        }
 
         void remove(int x){
-            
+            cout << "Attempting to remove " << x << " from the tree." << endl;
+            root = remove(x, root);
+            print();
+        }
+
+        void print(){
+            print(root, 0);
         }
 };
+
+int main(){
+    RefTree rtree;
+    rtree.add(50);
+    rtree.add(60);
+    rtree.add(30);
+    rtree.add(80);
+    rtree.add(70);
+    rtree.add(90);
+    rtree.add(75);
+    rtree.add(65);
+    rtree.add(55);
+    rtree.remove(60);
+    rtree.add(70);
+    rtree.remove(30);
+}
